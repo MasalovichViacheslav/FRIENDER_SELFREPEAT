c25323b1dad3a51f1401f5976673ed0ea5702a0c
Slava <Slavutichtich@gmail.com>
2023-04-17 17:05:44 +0300

    introduction to templates

--- friender/ARRANGEMENT/urls.py
+++ friender/ARRANGEMENT/urls.py
@@ -2,9 +2,13 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('time/', current_datetime),
-    path('greeting/', greeting),
-    path('<int:year>/', app_creation_year),
-    path('<slug:app_date>/', app_creation_date),
-    re_path(r'\d{4}.\d{2}.\d{2}', site_creation_date),
+    # path('time/', current_datetime),
+    # path('greeting/', greeting),
+    # path('<int:year>/', app_creation_year),
+    # path('<slug:app_date>/', app_creation_date),
+    # re_path(r'\d{4}.\d{2}.\d{2}', site_creation_date),
+    # path('example/', show_example),
+    path('main/', main_page, name="HOME_PAGE"),
+    path('users/', users, name="USERS_PAGE"),
+    path('places/', places, name="PLACES_PAGE"),
 ]
--- friender/ARRANGEMENT/views.py
+++ friender/ARRANGEMENT/views.py
@@ -1,25 +1,45 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 import datetime
 
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = f"<html><body>It is {now}.</body></html>"
-    return HttpResponse(html)
 
-def greeting(request):
-    return HttpResponse("Hello, Django!!!")
+def main_page(request):
+    return render(request, 'main.html')
 
-def app_creation_year(request, year):
-    return HttpResponse(f'This APP was created in {year}')
 
-def app_creation_date(request, app_date):
-    return HttpResponse(f'This APP was created on {app_date}')
+def users(request):
+    user_list = {
+        'Andrei': [25, 'male', 'Minsk'],
+        'Dmitri': [27, 'male', 'Minsk'],
+        'Pavel': [23, 'male', 'Orsha'],
+        'Olga': [24, 'female', 'Minsk'],
+        'Yulia': [22, 'female', 'Minsk'],
+        'Anna': [21, 'female', 'Vitebsk']
+    }
+    return render(request, 'users.html', context={'users': user_list})
 
-def site_creation_date(request):
-    return HttpResponse(f'This site was created on 2023-04-12')
+def places(request):
+    return render(request, 'places.html')
+
+# def current_datetime(request):
+#     now = datetime.datetime.now()
+#     html = f"<html><body>It is {now}.</body></html>"
+#     return HttpResponse(html)
+
+# def greeting(request):
+#     return HttpResponse("Hello, Django!!!")
+
+# def app_creation_year(request, year):
+#     return HttpResponse(f'This APP was created in {year}')
+
+# def app_creation_date(request, app_date):
+#     return HttpResponse(f'This APP was created on {app_date}')
+
+# def site_creation_date(request):
+#     return HttpResponse(f'This site was created on 2023-04-12')
+
+# def show_example(request):
+#     return render(request, "example.html", {'first_name': 'John', 'last_name': 'Doe'})
 
 
 
